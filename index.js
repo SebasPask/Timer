2e41ec095fc13649e54fac279f25416009bb4ede
SebasPask <sebaspask@gmail.com>
2017-10-13 16:47:53 +1100

    Update
    External information has been deleted

---
+++ index.js
@@ -0,0 +1,36 @@
+const electron = require('electron');
+const {app,BrowserWindow,Tray} = electron;
+const path = require('path');
+let mainWindow;
+let tray;
+
+app.on('ready',()=>{
+   mainWindow = new BrowserWindow({
+       height:500,
+       width:300,
+       frame:false,
+       resizable:false,
+       show:false
+   });
+   mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+
+   const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
+   const iconPath = path.join(__dirname,`./src/assets/${iconName}`);
+   tray = new Tray(iconPath);
+
+   tray.on('click',(event, bounds)=>{
+       const { x,y } = bounds;
+       const { height, width } = mainWindow.getBounds();
+       if(mainWindow.isVisible()){
+        mainWindow.hide();
+       }else{
+           mainWindow.setBounds({
+               x: x - width / 2 ,
+               y,
+               height,
+               width
+           });
+         mainWindow.show();
+       }
+   });
+});
