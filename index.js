840ac88190da7f3416fbbd85542668bcb6761358
SebasPask <sebaspask@gmail.com>
2017-10-13 20:51:31 +1100

    full performance in the background

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const electron = require('electron');
-const {app} = electron;
+const {app, ipcMain } = electron;
 const path = require('path');
 const TimerTray = require('./app/timer_tray');
 const MainWindow = require('./app/main_window');
@@ -14,3 +14,6 @@ app.on('ready',()=>{
    const iconPath = path.join(__dirname,`./src/assets/${iconName}`);
    tray = new TimerTray(iconPath , mainWindow);
 });
+    ipcMain.on('update-timer',(event,timeLeft) =>{
+       tray.setTitle(timeLeft);
+    });
